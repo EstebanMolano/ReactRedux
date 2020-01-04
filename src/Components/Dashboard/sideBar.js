e53fb2436ae9a65739d5970f4c436291ec0fd25f
EstebanMolano <31315118+EstebanMolano@users.noreply.github.com>
2020-01-04 12:18:49 -0500

    Add files via upload

--- src/Components/Dashboard/sideBar.js
+++ src/Components/Dashboard/sideBar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './sideBar.css';
+import { GoAlert } from "react-icons/go";
+import { TiDocument } from "react-icons/ti";
+import { FiShoppingCart } from "react-icons/fi";
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import ProjectFormList from "../ProjectFormList";
 import ProjectFormOrder from "../ProjectFormOrder";
@@ -7,53 +10,41 @@ import ProjectFormOrder from "../ProjectFormOrder";
 class sideBar extends Component {
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div class="wrapper">
                 <Router>
-                <nav id="sidebar">
-                <div class="sidebar-header">
-                    <h3>Challenger React</h3>
-                    <strong>BS</strong>
-                </div>
+                    <nav id="sidebar">
+                        <div class="sidebar-header">
+                        <p class="font-italic">Challenger React</p>
+                        </div>
+                        <ul class="list-unstyled components">
+                            <li>
+                                <TiDocument/>
+                                <a href="#">
+                                    <Link to="/">List of Products</Link>
+                                </a>
 
-                
-                <ul class="list-unstyled components">
-                    <li>
-                        <a href="#">
-                            <i class="glyphicon glyphicon-paperclip"></i>
-                            <Link to="/">Home</Link>
-                        </a>
-                        
-                    </li>
-                    
-                    <li>
-                        <a href="#">
-                            <i class="glyphicon glyphicon-paperclip"></i>
-                            <Link to="/orderer">Products</Link>
-                        </a>
-                    </li>
-                    
-                </ul>
-            
-           
-         
-          </nav>
-
-      
-        <div id="content">
-
-            <Switch>
-            <Route exact path="/" component={ProjectFormList}/>
-            <Route exact path="/orderer" component={ProjectFormOrder}/>
-          </Switch>
-             </div>
-        </Router>
+                            </li>
+                            <li> 
+                                <FiShoppingCart/>
+                                <a href="#">
+                                    <Link to="/orderer">Products Orderer</Link>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div id="content">
+                        <Switch>
+                            <Route exact path="/" component={ProjectFormList} />
+                            <Route exact path="/orderer" component={ProjectFormOrder} />
+                        </Switch>
+                    </div>
+                </Router>
             </div>
-            
+
         );
     }
-
 }
 
 export default sideBar;
